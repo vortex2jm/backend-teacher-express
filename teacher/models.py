5953fc99a15f53c3d468cff9ce3a84cda65300c3
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-09 13:08:00 -0300

    :recycle: Fixing max_lenght error

--- teacher/models.py
+++ teacher/models.py
@@ -6,8 +6,8 @@ class Teacher(models.Model):
   
   name = models.CharField(max_length=100, null=False, blank=False)
 
-  value = models.DecimalField(max_digits=0, decimal_places=2, null=False, blank=False)
+  value = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
 
   description= models.TextField(null=False, blank=False)
 
-  photo = models.URLField(max_lenght=255, null=False, blank=False)
+  photo = models.URLField(null=False, blank=False)

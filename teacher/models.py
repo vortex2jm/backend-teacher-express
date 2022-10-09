1b1e1329ee0269bfc39fa1804eb5b8fa56afb5a2
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-09 13:04:51 -0300

    :sparkles: Creating teacher's model

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+  
+  name = models.CharField(max_length=100, null=False, blank=False)
+
+  value = models.DecimalField(max_digits=0, decimal_places=2, null=False, blank=False)
+
+  description= models.TextField(null=False, blank=False)
+
+  photo = models.URLField(max_lenght=255, null=False, blank=False)

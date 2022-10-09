2447fd569446322c529ea349ff06fbe0a4379468
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-09 20:38:39 -0300

    :sparkles: Creating serializer to register teacher

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -5,3 +5,9 @@ class TeacherSereializer(serializers.ModelSerializer):
   class Meta:
     model = Teacher
     fields = "__all__"
+
+class RegisterTeacherSerializer(serializers.Serializer):
+  name = serializers.CharField(max_length=255)
+  value = serializers.DecimalField(max_digits=10, decimal_places=2)
+  description = serializers.CharField()
+  photo = serializers.CharField()

f01316f77c9e05b4915254341478453a65e5e86d
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-09 19:10:44 -0300

    :sparkles: Adding route to find all teachers

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView 
+from rest_framework.views import Response
+from teacher.models import Teacher
+from teacher.serializers import TeacherSereializer
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+class TeacherView(APIView):
+  def get(self, request, format=None):
+    teachers = Teacher.objects.all()
+    serializer = TeacherSereializer(teachers, many=True)
+    return Response(serializer.data, status=HTTP_200_OK)
--- teacher_express/urls.py
+++ teacher_express/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from teacher.views import TeacherView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('findAll', TeacherView.as_view())
 ]

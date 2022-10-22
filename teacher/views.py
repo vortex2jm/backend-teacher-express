328d8768f2d7048ac81638253b4bc1605f9befc2
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-22 14:58:15 -0300

    :sparkles: Adding delete route

--- teacher/views.py
+++ teacher/views.py
@@ -4,6 +4,8 @@ from rest_framework.views import Response
 from teacher.models import Teacher
 from teacher.serializers import (TeacherSereializer, RegisterTeacherSerializer)
 from rest_framework.status import (HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST)
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 class TeacherView(APIView):
   def get(self, request, format=None):
@@ -24,3 +26,12 @@ class TeacherView(APIView):
       register_teacher_serializer = RegisterTeacherSerializer(teacher, many=False)
       return Response(register_teacher_serializer.data, status=HTTP_201_CREATED)
     return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+  @csrf_exempt
+  def delete(self, request, id):
+    teacher = get_object_or_404(Teacher, pk=id)
+    teacher.delete()
+    res = f"Teacher {teacher.name} deleted sucefully"
+    return Response(res, HTTP_200_OK)
+
+# Estes métodos poderiam ser implementados fora de uma classe e chamados no url pattern
--- teacher_express/urls.py
+++ teacher_express/urls.py
@@ -4,5 +4,6 @@ from teacher.views import TeacherView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teachers', TeacherView.as_view())
+    path('teachers', TeacherView.as_view()),
+    path('delete-teachers/<int:id>', TeacherView.as_view())
 ]
